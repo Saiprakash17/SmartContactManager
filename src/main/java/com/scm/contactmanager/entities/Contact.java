812f17799cb27f3ca2644b140372c1b2bdf51ac9
Saiprakash Bollam <saiprakash.bollam17@gmail.com>
2025-03-03 20:00:17 -0500

    SignUp page created

--- src/main/java/com/scm/contactmanager/entities/Contact.java
+++ src/main/java/com/scm/contactmanager/entities/Contact.java
@@ -7,6 +7,7 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -29,6 +30,7 @@ import lombok.Setter;
 public class Contact {
 
     @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private int id;
     private String name;    
     private String email;
