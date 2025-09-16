87ae0a1d50354055410033281810086505a7cfe9
Saiprakash Bollam <saiprakash.bollam17@gmail.com>
2025-09-15 21:39:21 -0400

    fix contact service test

--- src/test/java/com/scm/contactmanager/helper/TestSessionHelper.java
+++ src/test/java/com/scm/contactmanager/helper/TestSessionHelper.java
@@ -1,10 +1,5 @@
 package com.scm.contactmanager.helper;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-
-@Component("sessionHelper")
-@Primary
 public class TestSessionHelper extends SessionHelper {
     @Override
     public void removeMessage() {
